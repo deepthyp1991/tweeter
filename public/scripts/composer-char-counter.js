471bf52b67fd6d70eff1fcb26a3786f3f026c16e
deepthyp1991 <deepthyp1991@gmail.com>
2020-12-16 14:22:38 +0000

    Implemented the counter part

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $('#tweet-text').on('input', function() {
+    let counter = $("#tweet-text").val().length;
+    if (counter <= 140) {
+      $('.counter').removeClass("change");
+      $('.counter').val(counter);
+    } else {
+      $('.counter').addClass('change');
+      $('.counter').val( 140 - counter);
+    }
+  })
+});
+
